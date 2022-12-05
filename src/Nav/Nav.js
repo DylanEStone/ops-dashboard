ed20dbe750dd31f64b27251e20c947b43d3c73bf
Dylan Stone <dylanevanstone@gmail.com>
2022-12-04 21:56:20 -0800

    Added React-Icon
    Got an icon to work for the time being. Still having trouble styling.

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,12 +1,12 @@
+import './Nav.css';
 import { Navbar,  Text } from "@nextui-org/react";
-import { DSNLogo } from "./DSNLogo.js"
-{/*import { Button, Link } from "@nextui-org/react";*/}
+import { GiOrbit } from "react-icons/gi";
 
 export default function Nav() {
   return (
     <Navbar isCompact isBordered variant="sticky">
       <Navbar.Brand>
-        <DSNLogo />
+        <GiOrbit />
         <Text b color="inherit" hideIn="xs">
           Deep Space Network Dashboard
         </Text>
